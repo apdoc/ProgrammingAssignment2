09c76f8cd8c776c47d25bebe61d00ad9e78380cd
unknown <tjo201@PHYL-CDT06.isad.isadroot.ex.ac.uk>
2014-10-21 12:36:20 +0100

    added code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCacheMatrix function creates a lists of functions which:
+# 1) set: sets the value of a matrix
+# 2) get: gets the value of a matrix
+# 3) set_inverse: sets the inverse of a matrix
+# 4) get_inverse: gets the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        i <- NULL                                       # i and x must be defined inside the function to stop the <<- operator                                                                              
+        set <- function(y) {                            # in the set function from creating i & x in the global environment
+                x <<- y                                 
+                i <<- NULL
+        }
+        
+        get <- function() x
+        set_inverse <- function(inverse) i <<- inverse
+        get_inverse <- function() i
+        
+        list(set = set, get = get,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve computes the inverse of a matrix, 
+# or collects it from memory if the calculation has been done before.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$get_inverse()
+        
+        if(!is.null(i)) {                               # The data is obtained from memory ending the function by
+                message("obtaining cached data")        # returning the inverse matrix if previously calculated
+                return(i)
+        }
+        
+        data <- x$get()
+        i <- solve(data, ...)
+        x$set_inverse(i)
+        
+        i
 }
+
+# An example of the working code is shown below:
+
+# > z <- makeCacheMatrix(matrix(1:4, 2, 2))
+# > cacheSolve(z)
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
+# If cacheSolve(z) is called a second time, then it displays a message
+# stating that the value is 'obtained from cached data' and the inverse
+# matrix is not computed again, but obtained straight from memory
+
+# > cacheSolve(z)
+# obtaining cached data
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
